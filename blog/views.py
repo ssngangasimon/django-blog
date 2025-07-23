ed3d11045d0c5f4fd228dc3ded16d103a7c2c4ef
Simon Nganga <ngangasimoncs@gmail.com>
2025-07-23 14:19:36 +0300

    add dynamic views, templates, and basic styling

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
+# Home page – list all posts
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
 
 def about(request):
     return render(request, 'blog/about.html')
+
+# Detail page – view one post by ID
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
